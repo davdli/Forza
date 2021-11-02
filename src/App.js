24c0b6a36e44d6e4f2715eecd4c679ec037792c0
David Li <dvdli2323@gmail.com>
2021-11-02 16:33:08 -0400

    adds cards

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+import Details from './components/Details/Details'
+
+const App = () => {
+  return (
+    <div>
+      <Grid container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          Main
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core'
+import { Doughnut } from 'react-chartjs-2'
+import useStyles from './styles'
+
+const Details = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.income}>
+      <CardHeader title="Income" />
+      <CardContent>
+        <Typography variant="h5">$50</Typography>
+        {/* <Doughnut data="Data" /> */}
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Details
+
