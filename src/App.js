bbee4a229949762cf74f536ad577ac81fa9a8e5b
David Li <dvdli2323@gmail.com>
2021-11-04 18:25:57 -0400

    displays navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import Main from './components/Main/Main';
 import Details from './components/Details/Details';
+import Nav from './components/Nav/Nav'
 import useStyles from './styles';
 
 const App = () => {
   const classes = useStyles();
   return (
     <div>
+      <Grid>
+          <Nav />
+        </Grid>
       <Grid
         className={classes.grid}
         container spacing={0}
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <p>Forzo</p>
+    </nav>
+  )
+}
+
+export default Nav;
