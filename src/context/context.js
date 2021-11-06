0f684bbb9b6bccc5401c4d6492f5884773044584
David Li <dvdli2323@gmail.com>
2021-11-06 17:25:40 -0400

    show new users some data

--- src/context/context.js
+++ src/context/context.js
@@ -4,7 +4,7 @@
 import React, { useReducer, createContext } from 'react';
 import contextReducer from './contextReducer';
 
-const initialState = JSON.parse(localStorage.getItem('inputs')) || []; //pull inputs immedicately from local storage
+const initialState = JSON.parse(localStorage.getItem('inputs')) || [{"category":"Current","exercise":"Bench","weight":225,"date":"2021-11-06","id":"4a75704c-20e9-47ae-8f86-b2dbedb54f74"},{"category":"Start","exercise":"Bench","weight":135,"date":"2021-11-06","id":"227541a8-b44f-4400-a974-aa41b178413a"}]; //pull inputs immedicately from local storage
 export const StrengthProgressContext = createContext(initialState);
 
 export const Provider = ({ children }) => {
