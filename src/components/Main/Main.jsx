9a4ed4f1111da417dcc15fdc239382c6d0fdf866
David Li <dvdli2323@gmail.com>
2021-11-06 17:41:58 -0400

    displays total gain from start to current

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider} from '@material-ui/core';
 import useStyles from './styles';
 import Form from './Form/Form';
 import List from './List/List';
+import { StrengthProgressContext } from '../../context/context';
 
 const Main = () => {
   const classes = useStyles();
+  const { totalGain } = useContext(StrengthProgressContext);
   return (
     <Card classname={classes.root} style={{backgroundColor: '#f4f4f4', borderRadius: '2rem', boxShadow: '0.5rem 0.5rem 1rem #18191b'}}>
       <CardHeader title="Strength Progress" />
       <CardContent>
-        <Typography align="center" variant="h5">Total Gain: 100 lb</Typography>
+        <Typography align="center" variant="h5">Total Gain: {totalGain} lb</Typography>
         <Typography variant="subtitle1" style={{lineHeight: '1.5em', marginTop: '20px'}} >
           {/* InfoCard... */}
           HEY FORZA: ADD TO CURRENT, 225 FOR BENCH
--- src/context/context.js
+++ src/context/context.js
@@ -4,7 +4,7 @@
 import React, { useReducer, createContext } from 'react';
 import contextReducer from './contextReducer';
 
-const initialState = JSON.parse(localStorage.getItem('inputs')) || [{"category":"Current","exercise":"Bench","weight":225,"date":"2021-11-06","id":"4a75704c-20e9-47ae-8f86-b2dbedb54f74"},{"category":"Start","exercise":"Bench","weight":135,"date":"2021-11-06","id":"227541a8-b44f-4400-a974-aa41b178413a"}]; //pull inputs immedicately from local storage
+const initialState = JSON.parse(localStorage.getItem('inputs')) || [{"category":"Start","exercise":"Bench","weight":135,"date":"2021-11-06","id":"4a75704c-20e9-47ae-8f86-b2dbedb54f74"},{"category":"Current","exercise":"Bench","weight":225,"date":"2021-11-06","id":"227541a8-b44f-4400-a974-aa41b178413a"}]; //pull inputs immedicately from local storage
 export const StrengthProgressContext = createContext(initialState);
 
 export const Provider = ({ children }) => {
@@ -22,11 +22,17 @@ export const Provider = ({ children }) => {
       payload: id
     })
   };
+
+  const totalGain = inputs.reduce((accum, val) => {
+    return (val.category === 'Start' ? accum - val.weight : accum + val.weight)
+  }, 0)
+
   return (
     <StrengthProgressContext.Provider value={{
       addInput,
       removeInput,
-      inputs
+      inputs,
+      totalGain
     }}>
       {children}
     </StrengthProgressContext.Provider>
